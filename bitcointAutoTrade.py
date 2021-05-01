44c37ffcbebec702997f18a1de40a0bea48d40ec
mnbvcxxxz <mnbvcxxxz89@gmail.com>
2021-05-01 22:33:12 +0900

    5월1일 진행

--- bitcointAutoTrade.py
+++ bitcointAutoTrade.py
@@ -5,17 +5,17 @@ import requests
 
 access = "NoRVfwch6I71UZHCZq3ji9Z5xcXz8r7U2QnxlGQn"
 secret = "J7t7vx31yrnbETxKroRJ6M1nyiddgGR571UL1YfG"
-myToken = "xoxb-your-token"
+myToken = "xoxb-2022008201107-2018729025317-bVjaXVu5ycIWBEd0qqQcQQTh" #슬랙토큰
 
 # 코인 정의
 Etherium = "KRW-ETH"
 Bitcoin = "KRW-BTC"
 
 # 매매 인터벌, 최근 조회데이터 수 정의
-intervals="minute30"
-counts=7
+intervals = "minute30"
+counts = 7
 
-#K값 세팅
+#K값 세팅 ->나중에 최적 K값 자동계산하도록
 K=0.3
 
 def post_message(token, channel, text):
@@ -67,25 +67,25 @@ post_message(myToken,"#crypto", "autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
-        end_time = start_time + datetime.timedelta(days=1)
+        start_time = get_start_time(Etherium)
+        end_time = start_time + datetime.timedelta(minutes=30) #days로 해야하나 minute으로 해야하나
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            ma15 = get_ma15("KRW-BTC")
-            current_price = get_current_price("KRW-BTC")
+            target_price = get_target_price(Etherium, K)
+            ma15 = get_ma15(Etherium)
+            current_price = get_current_price(Etherium)
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
-                    post_message(myToken,"#crypto", "BTC buy : " +str(buy_result))
+                    buy_result = upbit.buy_market_order(Etherium, krw*0.9995)
+                    post_message(myToken,"#거래-내역", "BTC buy : " +str(buy_result))
         else:
             btc = get_balance("BTC")
-            if btc > 0.00008:
-                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
-                post_message(myToken,"#crypto", "BTC buy : " +str(sell_result))
+            if btc > 0.00008: #현재 가격이 5천원일경우? 수정 필요할듯
+                sell_result = upbit.sell_market_order(Etherium, btc*0.9995)
+                post_message(myToken,"#거래-내역", "BTC buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
-        post_message(myToken,"#crypto", e)
+        post_message(myToken,"#거래-내역", e)
         time.sleep(1)
