0abf0cfa5ad4fcfdc36c963b285177e11e24a21c
mnbvcxxxz <mnbvcxxxz89@gmail.com>
2021-05-01 19:52:21 +0900

    5월1일 진행

--- test.py
+++ test.py
@@ -9,43 +9,76 @@ access = "NoRVfwch6I71UZHCZq3ji9Z5xcXz8r7U2QnxlGQn"          # 본인 값으로 
 secret = "J7t7vx31yrnbETxKroRJ6M1nyiddgGR571UL1YfG"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
-print(upbit.get_balance("KRW-ETH"))     # KRW-XRP 조회
+# 코인 정의
+Etherium = "KRW-ETH"
+Bitcoin = "KRW-BTC"
+
+# 매매 인터벌, 최근 조회데이터 수 정의
+intervals="minute30"
+counts=7
+
+#K값 세팅
+K=0.3
+
+
+print(upbit.get_balance(Etherium))     # KRW-XRP 조회
 print(upbit.get_balance("KRW"))         # 보유 현금 조회
 # print(pyupbit.get_tickers(fiat="KRW"))  # 특정Fiat로 매매 가능한 목록 조회
 #
-print(pyupbit.get_ohlcv("KRW-ETH", interval="minute30", count=30))
+print(pyupbit.get_ohlcv(Etherium, interval=intervals, count=counts))
 
 print("----------------------백테스팅-------------------")
 
 # OHLCV(open, high, low, close, volume)로 당일 시가, 고가, 저가, 종가, 거래량에 대한 데이터
 print("지난 데이터 추출")
 
-df = pyupbit.get_ohlcv("KRW-ETH", interval="minute30", count=7)
+df = pyupbit.get_ohlcv(Etherium, interval=intervals, count=counts)
 print(df)
 
 
 # 변동폭 * k 계산, (고가 - 저가) * k값
-print("변동폭 * 0.5 계산")
+print("k=0.3")
 
-df['range'] = (df['high'] - df['low']) * 0.5
+df['range'] = (df['high'] - df['low']) * K
+
 print(df['range'])
 
-# target(매수가), range 컬럼을 한칸씩 밑으로 내림(.shift(1))
-df['target'] = df['open'] + df['range'].shift(1)
+df['target'] = df['open'] + df['range'].shift(1) # target(목표매수가), range 컬럼을 한칸씩 밑으로 내림(.shift(1))
+
+print(df['target'])
 
 # ror(수익률), np.where(조건문, 참일때 값, 거짓일때 값)
 df['ror'] = np.where(df['high'] > df['target'],
                      df['close'] / df['target'],
                      1)
 
-# 누적 곱 계산(cumprod) => 누적 수익률
-df['hpr'] = df['ror'].cumprod()
 
-# Draw Down 계산 (누적 최대 값과 현재 hpr 차이 / 누적 최대값 * 100)
-df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+df['hpr'] = df['ror'].cumprod()  # 누적 곱 계산(cumprod) => 누적 수익률
 
-#MDD 계산
-print("MDD(%): ", df['dd'].max())
+df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100 # Draw Down 계산 (누적 최대 값과 현재 hpr 차이 / 누적 최대값 * 100)
+
+
+print("누적수익률(%):", df['hpr'])   #hpr 프린트
+print("MDD(%): ", df['dd'].max())   #MDD 계산/프린트
 
 #엑셀로 출력
 # df.to_excel("dd.xlsx")
+
+# 가장좋은 K값을 구하는 방법 (상승장일 경우K가 낮을수록, 하락장일수록 K가 높을수록 유리)
+print("가장 좋은 K값을 구하는 방법")
+def get_ror(k=K):
+    df = pyupbit.get_ohlcv(Etherium,interval=intervals, count=counts)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+
+for k in np.arange(0.1, 1.0, 0.1):
+    ror = get_ror(k)
+    print("%.1f %f" % (k, ror))
